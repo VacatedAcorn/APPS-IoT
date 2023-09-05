8e17b4fea38c78cf3f1661500a2727fe680bb3c1
VacatedAcorn <106562842+VacatedAcorn@users.noreply.github.com>
2023-09-05 15:07:37 -0600

    errores

--- Unidad 2/server.py
+++ Unidad 2/server.py
@@ -9,6 +9,10 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_header("Content-type", content_type)
         self.end_headers()
 
+    def throw_custom_error(self,message):
+        self._set_response("aplication/json")
+        self.wfile.write(json.dumps({"message": message}).encode())
+
     def do_GET(self):
         self._set_response()
         respuesta = {"valor": contador}
@@ -18,9 +22,27 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
         content_length = int(self.headers["Content-Length"])
         post_data = self.rfile.read(content_length)
 
-        body_json = json.loads(post_data.decode())
-
+        try:
+            body_json = json.loads(post_data.decode())
+        except:
+            self.throw_custom_error("Invalid JSON")
+            return
+            
         global contador
+
+        if (body_json.get('action') is None or body_json.get('quantity')is None):
+            self.throw_custom_error("Missing action or quantity")
+            return
+        
+        if (body_json['action'] != 'asc' and body_json['action'] != 'desc'):
+            self.throw_custom_error("Invalid action")
+            return
+        
+        try:
+            int(body_json['quantity'])
+        except:
+            self.throw_custom_error("Invalid quantity")
+            return
 
         if 'action' in body_json and 'quantity' in body_json:
             action = body_json['action']
