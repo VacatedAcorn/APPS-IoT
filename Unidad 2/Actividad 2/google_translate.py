bbaedd3a1edfaea496a58b560b70761768aab773
VacatedAcorn <106562842+VacatedAcorn@users.noreply.github.com>
2023-09-07 14:13:34 -0600

    traductor

--- Unidad 2/Actividad 2/google_translate.py
+++ Unidad 2/Actividad 2/google_translate.py
@@ -1,18 +1,17 @@
 import requests
 
+# get env
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+X_RAPIDAPI_KEY = os.getenv("X_RAPIDAPI_KEY")
+
 url = "https://google-translate1.p.rapidapi.com/language/translate/v2"
 
-payload = {
-	"q": "Hello, world!",
-	"target": "es",
-	"source": "en"
-}
-headers = {
-	"content-type": "application/x-www-form-urlencoded",
-	"Accept-Encoding": "application/gzip",
-	"X-RapidAPI-Key": "9ce6d17fabmsh3ba273d2ae819b1p1bd1c4jsnfeb32be0288a",
-	"X-RapidAPI-Host": "google-translate1.p.rapidapi.com"
-}
+payload = {"q": "Hello, furry world!", "target": "es", "source": "en"}
+headers = {"content-type": "application/x-www-form-urlencoded", "Accept-Encoding": "application/gzip", "X-RapidAPI-Key": X_RAPIDAPI_KEY, "X-RapidAPI-Host": "google-translate1.p.rapidapi.com"}
 
 response = requests.post(url, data=payload, headers=headers)
 
